d8e6d4e8d19864ad5c66065f2494931db4cccb7b
kroc400 <102080023+kroc400@users.noreply.github.com>
2026-05-18 20:18:00 +0300

    revert Adminka

--- admin/nav.php
+++
@@ -1,7 +0,0 @@
-<div class="admin-nav">
-    <a href="cars.php" class="<?= basename($_SERVER['PHP_SELF']) == 'cars.php' ? 'active' : '' ?>">Автомобили</a>
-    <a href="orders.php" class="<?= basename($_SERVER['PHP_SELF']) == 'orders.php' ? 'active' : '' ?>">Заказы</a>
-    <a href="reviews.php" class="<?= basename($_SERVER['PHP_SELF']) == 'reviews.php' ? 'active' : '' ?>">Отзывы</a>
-    <a href="users.php" class="<?= basename($_SERVER['PHP_SELF']) == 'users.php' ? 'active' : '' ?>">Пользователи</a>
-    <a href="../account.html" target="_blank">Перейти на сайт</a>
-</div>
